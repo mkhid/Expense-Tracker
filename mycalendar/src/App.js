a6452880be151b77b339c6a54f3f575f75932ffb
MKhid <king.yawson1@gmail.com>
2023-10-15 18:28:13 +0000

    changd the ui

--- mycalendar/src/App.js
+++ mycalendar/src/App.js
@@ -10,12 +10,12 @@ import axios from 'axios';
 const Container = styled.div`
   background-color: white;
   color: #0d1d2c;
-  display: flex;
-  flex-direction: column;
-  margin: 0 10px;
-  align-items: center;
-  height: 100vh;
-  width: 98%;
+  // display: flex;
+  // flex-direction: column;
+  // margin: 0 10px;
+  // align-items: center;
+  // height: 100vh;
+  // width: 98%;
   padding-top: 30px;
   font-family: Montserrat;
 `;
@@ -23,9 +23,8 @@ const Container = styled.div`
 const Header = styled.div`
   background-color: white;
   color: #0d1d2c;
-  display: flex;
-  flex-direction: row;
   align-items: center;
+  justify-content: center;
   padding: 22px;
   font-size: 25px;
   font-weight: bold;
